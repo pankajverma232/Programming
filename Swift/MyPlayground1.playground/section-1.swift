120946d0bac2883a1e74a287b7d3a3bfed73877a
Pankaj Verma <pankajverma232@gmail.com>
2015-12-01 00:36:11 +0530

    Update section-1.swift

--- Swift/MyPlayground1.playground/section-1.swift
+++ Swift/MyPlayground1.playground/section-1.swift
@@ -1,73 +1,42 @@
-// Playground - noun: a place where people can play
+/*
+------------ 0-1 Knapsack problem -----------
+Given weights and values of n items, put these items in a knapsack of capacity W
+to get the maximum total value in the knapsack. In other words, given two integer
+arrays val[0..n-1] and wt[0..n-1] which represent values and weights associated 
+with n items respectively. Also given an integer W which represents knapsack
+capacity, find out the maximum value subset of val[] such that sum of the weights
+of this subset is smaller than or equal to W. You cannot break an item, either 
+pick the complete item, or don’t pick it (0-1 property).
+*/
 
-import UIKit
-
-let dict = [
-    "even" : [2,4,6,8,10],
-    "odd" : [1,3,5,7],
-    "other" :[0]
-]
-for (key , values) in dict {
-    for i in values {
-        if i==0 {
-        print("element \(i) is Zero")
-            break
-        }
-        if i % 2 == 0 {
-        print("element \(i) is even")
-        }
-        else {
-        print("element \(i) is odd")
+func knapsack(wt:[Int],val:[Int],n:Int,W:Int)->Int{
+    var i:Int
+    var j:Int
+    var K = [[Int]]() 
+    // allocate memory
+   for i=0;i<=n;++i {
+        var subArray = [Int]()
+       for j=0;j<=W;++j{
+           subArray.append(0)
+       }
+       K.append(subArray)
+   }
+   
+    for i=0;i<=n;++i {
+        for j=0;j<=W;++j{
+            if i==0 || j==0 {
+              continue
+            }
+            if wt[i-1] > j  {
+                K[i][j] = K[i-1][j]
+            }
+            else {
+                K[i][j] = max(K[i-1][j-wt[i-1]] + val[i-1],K[i-1][j])
+            }
         }
     }
+    return K[n][W]
 }
-
-
-
-func calcState(score : [Int]) -> (min:Int,max:Int,sum:Int){
-var min = score[0]
-    var max = score[0]
-    var sum = 0
-    for s in score {
-        if s > max {
-        max = s
-        }
-        else if s < min {
-        min = s
-        }
-sum += s
-    }
-return (min,max,sum)
-}
-
-let stat=calcState([5,3,100,3,9])
-stat.sum
-stat.2
-stat.min
-stat.max
-stat.1
-stat.0
-
-
-
-
-
-/*___________________________________________________________________*/
-
-//func myReduce<U ,T>(initial: U, combine: (U, T) -> U) -> U
-extension Array {
-    func myReduce<T, U>(seed:U, combiner:(U, T) -> U) -> U {
-        var current = seed
-        for item in self {
-            current = combiner(current, item as! T)
-        }
-        return current
-    }
-}
-
-var evenSum = Array(1...10)
-    .filter { (number) in number % 2 == 0 }
-    .myReduce(0) { (total, number) in total + number }
-
-print(evenSum)
-
+var wt:[Int] = [20,30,50]
+var val:[Int] = [60,120,250]
+ print("max profit : \(knapsack(wt,val:val,n:3,W:50)) \n")
