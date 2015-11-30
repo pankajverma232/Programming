668ea133fb6a1690e4ce09c429a266cdcb998a7b
Pankaj Verma <pankajverma232@gmail.com>
2015-12-01 01:20:08 +0530

    Update section-1.swift

--- Swift/MyPlayground1.playground/section-1.swift
+++ Swift/MyPlayground1.playground/section-1.swift
@@ -40,3 +40,38 @@ func knapsack(wt:[Int],val:[Int],n:Int,W:Int)->Int{
 var wt:[Int] = [20,30,50]
 var val:[Int] = [60,120,250]
  print("max profit : \(knapsack(wt,val:val,n:3,W:50)) \n")
+ 
+ /*
+ ------------ LIS problem ----------------
+ 
+ The longest Increasing Subsequence (LIS) problem is to find the length of the 
+ longest subsequence of a given sequence such that all elements of the subsequence
+ are sorted in increasing order.
+ For example, length of LIS for { 10, 22, 9, 33, 21, 50, 41, 60, 80 } is 6 
+ and LIS is {10, 22, 33, 50, 60, 80}.
+ */
+
+ func LIS(arr:[Int],n:Int)->Int{
+     var max = 0;
+     var K = [Int]()
+     for var i=0;i<n;++i {
+         K.append(1);
+     }
+     for var i=1;i<n;++i{
+         for var j=0;j<i;++j{
+             if arr[i]>arr[j] && K[j]+1 > K[i]{
+                 K[i] = K[j]+1
+             }
+         }
+     }
+     
+     for var i=0;i<n;i++ {
+         if max < K[i]{
+             max = K[i]
+         }
+     }
+     return max
+ }
+ var arr:[Int] = [10, 22, 9, 33, 21, 50, 41, 60]
+ 
+  print("LIS = \(LIS(arr,n:8))")
